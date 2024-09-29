e0619f6855b0d46e4c30fd21a3c773a8df02b2a2
Ashwani kumar pandey <aahwanikumarpandey@gmail.com>
2024-09-29 19:42:16 +0530

    Basics of python

---
+++ firstprogram.py
@@ -0,0 +1,153 @@
+name = "Ashwani" #string
+age = 22 #integer
+price = 34.44 # float
+age2 = age
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+# aritmetic operator
+
+a = 6
+b = 2
+sum = a+b
+print(sum)
+a = 8
+b = 7
+diff = a-b
+print(diff)
+
+print(a*b)
+print(a/b)
+print(a % b) #remainder
+print(a**b) #a^b 
+
+#relational operator
+
+# a = 30
+# b = 20
+
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=ab)
+# print(a<b)
+# print(a>b)
+
+#assignment operator
+
+# num = 10
+# #num = num + 10 #10+10  =>20
+# #num += 10
+# print("num :", num)
+# #num -= 10
+# print("num :", num)
+# #num *= 10
+# print("num :", num)
+# #num /= 10
+# print("num :", num)
+# num = 10
+# #num %= 4
+# print("num :", num)
+# num = 2
+# num **= 4
+# print("num =", num)
+
+
+
+#logical operator (not, and, or)
+
+# a = 80
+# b = 65
+# print(not {a > b})
+# print(not False)
+
+# val1 = True
+# val2 = False
+
+# print("ans operator :", val1 and val2)
+
+# print("OR Operator :", val1 or val2)
+
+# a = 30
+# b = 20
+
+# print("or operator :", a==b or a>b)
+
+# TYPE CONVERSUON
+
+# a = "2"
+# b = 4.25
+# sum = a + b #can only concatenate str (not "float") to str
+# print(a + b)
+
+# a = int("2")
+# b = 4.25
+# print(type(a))
+# print(a + b)
+
+# a = float("ASHWANI") #float and string not 
+# b = 4.25
+# print(type(a))
+# print(a + b)
+
+
+# INPUT IN PYTHON
+
+# name = input("enter your name :")
+# print("welcome", name)
+
+# name = input("enter your name :")
+# age = input("enter your age :")
+# marks = input("enter your marks :")
+#                                      # all answer in on string
+# print("welcome", name)
+# print("age =", age)
+# print("marks =", marks)
+
+
+# name = input("enter your name :")
+# age =  int(input("enter your age :"))
+# marks = float(input("enter your marks :"))
+                                    
+# print("welcome", name)    # if not define variable all answer type is string
+# print("age =", age)
+# print("marks =", marks)
+
+
+# #PRACTIC
+
+# # first = int(input("e first :"))
+# # second = int(input("e second :"))
+# # print("sum =", first + second)
+
+# # side = float(input("enter square side : "))
+
+# # print("area =",side **2)
+
+
+# # a = float(input("enter first : "))
+# # b = float(input("enter first : "))
+
+# # print ("avg =", (a+b)/2)
+
+
+# a = int(input("enter first : "))
+# b = int(input("enter first : "))
+
+# print(a >=b )
+
+
+
+
+
+
+
+
+
+
+
+
+
